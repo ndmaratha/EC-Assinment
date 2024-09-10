74c491b6ae78d43eb93c6ff2f428f9c6ab468ed2
Nayan Desai <ndmaratha01@gmail.com>
2024-09-10 22:44:49 +0530

    changes made check

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App, { appRouter } from "./App.tsx";
+import { appRouter } from "./App.tsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 
