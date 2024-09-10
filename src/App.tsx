d44618864a581488469884064535496582a3bfb2
Nayan Desai <ndmaratha01@gmail.com>
2024-09-10 22:39:29 +0530

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { createBrowserRouter, Outlet } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Signup from "./Component/Signup";
 import Login from "./Component/Login";
 import Dashboard from "./Component/Dashboard";
 
 function App() {
-	return (
-		<>
-			<Dashboard />
-		</>
-	);
+	return <></>;
 }
 
 export const appRouter = createBrowserRouter([
 	{
 		path: "/",
-		element: <App />,
+		element: <Dashboard />,
 	},
 	{
 		path: "/signup",
